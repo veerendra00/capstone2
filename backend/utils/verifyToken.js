048e529ffc04f9c3d4b5da32d8d18ac6249f74b0
veerendra <veera44@gmail.com>
2023-12-15 22:27:44 +0530

    chenges for cookies

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -4,7 +4,7 @@ import Jwt from 'jsonwebtoken';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken
     if (!token) {
-        return res.status(401).json({ success: false, message: "You're not authorize" })
+        return res.status(401).json({ success: false, message: "You're not authorize !" })
     }
 
     //if token isexist then verify the token
